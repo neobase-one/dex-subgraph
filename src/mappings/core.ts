e052cdb1be124bd3f40060ea349d3ad19149be73
akshatcx <akshat.c2k@gmail.com>
2022-09-14 19:40:06 +0530

    update to use stableswap on canto

--- src/mappings/core.ts
+++ src/mappings/core.ts
@@ -31,7 +31,8 @@ function isCompleteMint(mintId: string): boolean {
 
 export function handleTransfer(event: Transfer): void {
   // ignore initial transfers for first adds
-  if (event.params.to.toHexString() == ADDRESS_ZERO && event.params.value.equals(BigInt.fromI32(1000))) {
+  // if (event.params.to.toHexString() == ADDRESS_ZERO && event.params.value.equals(BigInt.fromI32(1000))) {
+  if (event.params.to.toHexString() == ADDRESS_ZERO && event.params.amount.equals(BigInt.fromI32(1000))) {
     return
   }
 
@@ -49,7 +50,8 @@ export function handleTransfer(event: Transfer): void {
   let pairContract = PairContract.bind(event.address)
 
   // liquidity token amount being transfered
-  let value = convertTokenToDecimal(event.params.value, BI_18)
+  // let value = convertTokenToDecimal(event.params.value, BI_18)
+  let value = convertTokenToDecimal(event.params.amount, BI_18)
 
   // get or create transaction
   let transaction = Transaction.load(transactionHash)
--- src/mappings/tokenDefinition.ts
+++ src/mappings/tokenDefinition.ts
@@ -22,59 +22,113 @@ export class TokenDefinition {
   static getStaticDefinitions(): Array<TokenDefinition> {
     let staticDefinitions = new Array<TokenDefinition>(6)
 
-    // Add DGD
-    let tokenDGD = new TokenDefinition(
-      Address.fromString('0xe0b7927c4af23765cb51314a0e0521a9645f0e2a'),
-      'DGD',
-      'DGD',
-      BigInt.fromI32(9)
-    )
-    staticDefinitions.push(tokenDGD)
+    // // Add DGD
+    // let tokenDGD = new TokenDefinition(
+    //   Address.fromString('0xe0b7927c4af23765cb51314a0e0521a9645f0e2a'),
+    //   'DGD',
+    //   'DGD',
+    //   BigInt.fromI32(9)
+    // )
+    // staticDefinitions.push(tokenDGD)
 
-    // Add AAVE
-    let tokenAAVE = new TokenDefinition(
-      Address.fromString('0x7fc66500c84a76ad7e9c93437bfc5ac33e2ddae9'),
-      'AAVE',
-      'Aave Token',
+    // // Add AAVE
+    // let tokenAAVE = new TokenDefinition(
+    //   Address.fromString('0x7fc66500c84a76ad7e9c93437bfc5ac33e2ddae9'),
+    //   'AAVE',
+    //   'Aave Token',
+    //   BigInt.fromI32(18)
+    // )
+    // staticDefinitions.push(tokenAAVE)
+
+    // // Add LIF
+    // let tokenLIF = new TokenDefinition(
+    //   Address.fromString('0xeb9951021698b42e4399f9cbb6267aa35f82d59d'),
+    //   'LIF',
+    //   'Lif',
+    //   BigInt.fromI32(18)
+    // )
+    // staticDefinitions.push(tokenLIF)
+
+    // // Add SVD
+    // let tokenSVD = new TokenDefinition(
+    //   Address.fromString('0xbdeb4b83251fb146687fa19d1c660f99411eefe3'),
+    //   'SVD',
+    //   'savedroid',
+    //   BigInt.fromI32(18)
+    // )
+    // staticDefinitions.push(tokenSVD)
+
+    // // Add TheDAO
+    // let tokenTheDAO = new TokenDefinition(
+    //   Address.fromString('0xbb9bc244d798123fde783fcc1c72d3bb8c189413'),
+    //   'TheDAO',
+    //   'TheDAO',
+    //   BigInt.fromI32(16)
+    // )
+    // staticDefinitions.push(tokenTheDAO)
+
+    // // Add HPB
+    // let tokenHPB = new TokenDefinition(
+    //   Address.fromString('0x38c6a68304cdefb9bec48bbfaaba5c5b47818bb2'),
+    //   'HPB',
+    //   'HPBCoin',
+    //   BigInt.fromI32(18)
+    // )
+    // staticDefinitions.push(tokenHPB)
+
+    // Add Note
+    let tokenNote = new TokenDefinition(
+      Address.fromString('0x4e71A2E537B7f9D9413D3991D37958c0b5e1e503'),
+      'NOTE',
+      'Note',
       BigInt.fromI32(18)
-    )
-    staticDefinitions.push(tokenAAVE)
+    );
+    staticDefinitions.push(tokenNote);
 
-    // Add LIF
-    let tokenLIF = new TokenDefinition(
-      Address.fromString('0xeb9951021698b42e4399f9cbb6267aa35f82d59d'),
-      'LIF',
-      'Lif',
+    // Add USDC
+    let tokenUsdc = new TokenDefinition(
+      Address.fromString('0x80b5a32E4F032B2a058b4F29EC95EEfEEB87aDcd'),
+      'USDC',
+      'USDC',
+      BigInt.fromI32(6)
+    );
+    staticDefinitions.push(tokenUsdc);
+
+    // Add USDT
+    let tokenUsdt = new TokenDefinition(
+      Address.fromString('0xd567B3d7B8FE3C79a1AD8dA978812cfC4Fa05e75'),
+      'USDT',
+      'USDT',
+      BigInt.fromI32(6)
+    );
+    staticDefinitions.push(tokenUsdt);
+
+    // Add ATOM
+    let tokenAtom = new TokenDefinition(
+      Address.fromString('0xecEEEfCEE421D8062EF8d6b4D814efe4dc898265'),
+      'ATOM',
+      'ATOM',
+      BigInt.fromI32(6)
+    );
+    staticDefinitions.push(tokenAtom);
+
+    // Add ETH
+    let tokenEth = new TokenDefinition(
+      Address.fromString('0x5FD55A1B9FC24967C4dB09C513C3BA0DFa7FF687'),
+      'ETH',
+      'ETH',
       BigInt.fromI32(18)
-    )
-    staticDefinitions.push(tokenLIF)
+    );
+    staticDefinitions.push(tokenEth);
 
-    // Add SVD
-    let tokenSVD = new TokenDefinition(
-      Address.fromString('0xbdeb4b83251fb146687fa19d1c660f99411eefe3'),
-      'SVD',
-      'savedroid',
+    // Add wCANTO
+    let tokenWeth = new TokenDefinition(
+      Address.fromString('0x826551890Dc65655a0Aceca109aB11AbDbD7a07B'),
+      'wCANTO',
+      'wCANTO',
       BigInt.fromI32(18)
-    )
-    staticDefinitions.push(tokenSVD)
-
-    // Add TheDAO
-    let tokenTheDAO = new TokenDefinition(
-      Address.fromString('0xbb9bc244d798123fde783fcc1c72d3bb8c189413'),
-      'TheDAO',
-      'TheDAO',
-      BigInt.fromI32(16)
-    )
-    staticDefinitions.push(tokenTheDAO)
-
-    // Add HPB
-    let tokenHPB = new TokenDefinition(
-      Address.fromString('0x38c6a68304cdefb9bec48bbfaaba5c5b47818bb2'),
-      'HPB',
-      'HPBCoin',
-      BigInt.fromI32(18)
-    )
-    staticDefinitions.push(tokenHPB)
+    );
+    staticDefinitions.push(tokenWeth);
 
     return staticDefinitions
   }
