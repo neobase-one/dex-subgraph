d3021810374b9596d10d2d0766e06fbb94a7a936
Tushar Jain <>
2023-02-10 22:20:30 +0530

    update comment in code

--- src/mappings/factory.ts
+++ src/mappings/factory.ts
@@ -24,7 +24,7 @@ const PAIR_WHITELIST: string[] = [
 ]
 
 export function handleNewPair(event: PairCreated): void {
-  // ignore if pair in blacklist
+  // ignore if pair not in whitelise
   if(!PAIR_WHITELIST.includes(event.params.pair.toHexString())) {
     return
   }
